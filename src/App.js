f48b87c880eb96728a6048431ca527971313760b
michael <mnzube@gmail.com>
2020-07-05 23:45:04 +0200

    added signup forms and routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm  from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
@@ -30,6 +32,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
+      route:'signin',
+      isSignedIn: false
     }
   }
 
@@ -49,6 +53,7 @@ class App extends Component {
   displayFaceBox = (box) => {
     this.setState({box:box})
   }
+
    onInputChange = (event) => {
  this.setState({input: event.target.value})
   }
@@ -60,17 +65,36 @@ class App extends Component {
   this.displayFaceBox(this.calculateFaceLocation(response)))
   .catch(err => console.log(err))
   }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState ({isSignedIn:false })
+    } else if (route === 'home') {
+      this.setState ({isSignedIn: true})
+    }
+    this.setState ({route:route})
+  }
+
   render() {
+   const { isSignedIn, imageUrl, box, route } = this.state;
   return (
     <div className="App">
     <Particles className='particles' params={particlesOptions} />
-     <Navigation />
-     <Logo />
+     <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+     { this.state.route === 'home' 
+     ? <div><Logo />
      <Rank />
      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit= {this.onButtonSubmit}/>
-     <FaceRecognition box= {this.state.box} imageUrl={this.state.imageUrl}/>
+     <FaceRecognition box= {box} imageUrl={imageUrl}/>
+     </div>
+     : (
+    route === 'signin' ? 
+       <Signin onRouteChange={this.onRouteChange} /> 
+       : <Register onRouteChange={this.onRouteChange} /> 
+     )      
+     }
     </div>
   );
 }
 }
-export default App;
+export default App; 
